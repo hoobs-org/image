dee056d1f0bd820d4b66646aee7b0b13c0652984
mkellsy <mkellsy@gmail.com>
2022-03-01 19:54:11 -0700

    oops

--- config/stages/customize/02-hoobs/01-run.sh
+++ config/stages/customize/02-hoobs/01-run.sh
@@ -3,5 +3,5 @@
 install -v -m 755 "${SRC}/install.sh" "${ROOTFS_DIR}/tmp/"
 
 on_chroot << EOF
-/bin/bash -c "/tmp/install.sh $RELEASE $NODE_REPO $HOOBS_REPO $IMG_TYPE
+/bin/bash -c "/tmp/install.sh $RELEASE $NODE_REPO $HOOBS_REPO $IMG_TYPE"
 EOF
